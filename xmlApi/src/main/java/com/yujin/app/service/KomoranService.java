c4a69190eecb53755e05c12dee6b58bb0a0977bb
podotang <  >
2024-08-08 08:07:46 +0900

    gggggggg

--- xmlApi/src/main/java/com/yujin/app/service/KomoranService.java
+++ xmlApi/src/main/java/com/yujin/app/service/KomoranService.java
@@ -1,5 +1,6 @@
 package com.yujin.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -12,29 +13,60 @@ import kr.co.shineware.nlp.komoran.model.Token;
 @Service
 public class KomoranService {
 	
-	public void action() {
-		Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
-		String document = "이게 이렇게해서 될까 싶네";
+	public List<String> analyzeText(String document) {
+	    Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+	    String[] words = document.split(" ");
+	    List<String> result = new ArrayList<>();
+
+	    for (String word : words) {
+	        KomoranResult analyzeResultList = komoran.analyze(word);
+	        List<Token> tokenList = analyzeResultList.getTokenList();
+	        
+	        for (Token token : tokenList) {
+	            String pos = token.getPos();
+	            if (pos.equals("NNG") || pos.equals("NNP")) {
+	                result.add(token.getMorph());
+	            }
+	        }
+	    }
+	    return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+//	
+//	public void action() {
+//		Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
 //		String document = "반면에 한국어는 조사가 너무 많이 있고, 신조어도 많이 나오고 규칙이 명확하지 않기 때문에 외국에서 만든 기술로 한국어를 적용시킬 수 없다는 문제가 있습니다. ";
-		KomoranResult analyzeResultList = komoran.analyze(document);
-
-		System.out.println(analyzeResultList.getPlainText());
-
-		List<Token> tokenList = analyzeResultList.getTokenList();
-//		for (Token token : tokenList) {
-//			System.out.format("%2d, %2d,%s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(),
-//					token.getPos());
-//		}
-
-				for (Token token : tokenList) {
-			System.out.format("(%2d, %2d)%s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(),
-					token.getPos());
-		}
-		
-		List<String> nounList = analyzeResultList.getNouns();
-		for (String noun : nounList) {
-			System.out.println(noun);
-		}
-	} 
+//		
+////		KomoranResult analyzeResultList = komoran.analyze(document);
+////		System.out.println(analyzeResultList.getPlainText());
+////		List<Token> tokenList = analyzeResultList.getTokenList();
+////		for (Token token : tokenList) {
+////			System.out.format("(%2d, %2d)%s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(),
+////					token.getPos());
+////		}
+//		
+//	    String[] words = document.split(" ");
+//	    
+//	    for (String word : words) {
+//	        KomoranResult analyzeResultList = komoran.analyze(word);
+//	        List<Token> tokenList = analyzeResultList.getTokenList();
+//	        
+//	        for (Token token : tokenList) {
+//	        	String pos = token.getPos();
+//	            if (pos.equals("NNG") || pos.equals("NNP")) {
+////	            System.out.format("(%d, %d) %s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(), token.getPos());
+//	                System.out.println(token.getMorph());
+//
+//	            }
+//	        }
+//	    }
+//
+//	} 
 
 }
--- xmlApi/src/main/java/com/yujin/app/web/SampleController.java
+++ xmlApi/src/main/java/com/yujin/app/web/SampleController.java
@@ -1,5 +1,8 @@
 package com.yujin.app.web;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yujin.app.service.KomoranService;
+
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
 
 @Controller
 public class SampleController {
@@ -25,11 +32,11 @@ public class SampleController {
 		 return "/test/test2";
 	    }
 	 
-	 @GetMapping("komoran")
-	    public String komoran() {
-		 service.action();
-		 return "/test/komoran";
-	    }
+//	 @GetMapping("komoran")
+//	    public String komoran() {
+//		 service.action();
+//		 return "/test/komoran";
+//	    }
 	 
 	 @PostMapping("annyang")
 	 @ResponseBody
@@ -37,12 +44,31 @@ public class SampleController {
 		 return "/test/komoran";
 	 }
 	 
-	 @GetMapping("annyang")
-	    public String annyang() {
-		 return "/test/annyang";
-	    }
+//	 @GetMapping("annyang")
+//	    public String annyang() {
+//		 return "/test/annyang";
+//	    }
 
+	 @GetMapping("/analyze")
+	 public String getAnalyzePage() {
+	     return "test/annyang"; // GET 요청 시 반환할 페이지 또는 데이터
+	 }
 	 
+	 
+	 @ResponseBody
+	 @PostMapping("/analyze")
+	    public List<String> analyzeText(@RequestBody Map<String, String> payload) {
+	        return service.analyzeText(payload.get("text"));
+	    }
+	 
+//	 @ResponseBody
+//	 @PostMapping("/analyze")
+//	    public List<String> analyzeText(@RequestBody Map<String, String> payload) {
+//	        String text = payload.get("text");
+//	        Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+//	        KomoranResult result = komoran.analyze(text);
+//	        return result.getNouns();
+//	    }
 	 
 	 
 	 
